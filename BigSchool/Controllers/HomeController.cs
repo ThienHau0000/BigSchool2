2ab5e7823ef5cc8dc1476b430e1094d370f47e0c
Administrator <Administrator@ADMIN>
2023-03-22 23:57:54 +0700

    Add project files.

---
+++ BigSchool/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using BigSchool.Models;
+
+namespace BigSchool.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var upcommingCourse = _dbContext;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
